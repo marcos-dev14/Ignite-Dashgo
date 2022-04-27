f6765435cb1a3ca158801754397e478860c838d3
marcospaulodev <suporte@tgoo.pt>
2022-04-27 08:56:38 -0300

    Finalizando o Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiSearchLine, RiNotificationLine, RiUserAddFill } from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -52,7 +52,35 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20"/>
+      </Flex>
 
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddFill} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Marcos Paulo</Text>
+            <Text color="gray.300" fontSize="small">
+              mprogramador.10@gmail.com
+            </Text>
+          </Box>
+          
+          <Avatar size="md" name="Marcos Paulo" src="https://github.com/marcos-dev14.png" />
+        </Flex>
       </Flex>
     </Flex>
   );
